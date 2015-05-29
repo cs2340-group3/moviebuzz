ae27dd2c19b565207e35ed615aa7bb6cdd68757e
z3t1 <zhfyyq@gmail.com>
2015-05-29 14:36:43 -0400

    make database work. build basic file structure

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , app = express()
+  , mongoose = require('mongoose');
+
+// Import MongoDB configuration
+var DB = require('./config/database')
+
+// Use the configuration to connect database
+mongoose.connect(DB.url);
 
 app.get('/', function(req, res) {
   res.send('Hello, world!');
@@ -10,3 +17,15 @@ app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
 
+//var Schema = mongoose.Schema;
+//var BlogPostSchema = new Schema({
+  //title   : String
+  //, body  : String
+  //});
+//var BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+//var p = new BlogPost();
+//p.title = "Testing 2";
+//p.body = "Test body";
+//p.save(function(err) {
+  //if (!err) console.log('Success!');
+//})
---
+++ config/database.js
@@ -0,0 +1,5 @@
+var database = {
+  url: 'mongodb://localhost/test'
+}; // The default port of MongoDB is 27017
+
+module.exports = database;
