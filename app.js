b41ea54ff6c8a5645674706298658dd858e3aea6
Tim Vergenz <vergenzt@gmail.com>
2015-05-29 12:01:38 -0400

    Write hello world

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-// set up routes
+app.get('/', function(req, res) {
+  res.send('Hello, world!');
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
