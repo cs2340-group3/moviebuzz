77235640560a5f6ed96066dac016c79922dfa281
Tim Vergenz <vergenzt@gmail.com>
2015-05-29 12:00:49 -0400

    Fix apostrophes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on port " + port);
+  console.log('Listening on port ' + port);
 });
 
