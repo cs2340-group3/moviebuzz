3f110fb969a536389c87488c7734865100c4bed3
Tim Vergenz <vergenzt@gmail.com>
2015-06-01 14:42:06 -0400

    Use Mongolab URL if available

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 var database = {
-  url: 'mongodb://localhost/test'
+  url: process.env.MONGOLAB_URI || 'mongodb://localhost/test'
 }; // The default port of MongoDB is 27017
 
 module.exports = database;
