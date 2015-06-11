d9a80b4b76593496f255d8cda4b9bde76a435e34
Tian-Yo Yang <tyang82@gatech.edu>
2015-06-11 10:01:57 -0400

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,6 @@
-<link href="public/vendor/x-editable/dist/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">
-<script src="public/vendor/x-editable/dist/bootstrap3-editable/js/bootstrap-editable.js"></script>
-
-$('#username').editable({
-    type: 'text',
-    pk: 1,
-    url: '/post',
-    title: 'Enter username'
-
+$(document).ready(function() {
+    
+    $.fn.editable.defaults.mode = 'popup';     
+    
+    $('#username').editable();
 });
