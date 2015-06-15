8ee218ef03460cea779d93164bfc3d179d80707f
z3t1 <zhfyyq@gmail.com>
2015-06-15 11:30:51 -0400

    Added CSRF defense to PUT method in profile editing.

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
   };
   $.fn.editable.defaults.mode = 'inline'; // inline editing, no pop up
   $.fn.editable.defaults.send = 'always'; // we don't have pk field
+  $.fn.editable.defaults.params = function (params) {
+    params._csrf = $("#_token").data("token");
+    return params;
+  };
   $('#firstname').editable();
   $('#lastname').editable();
   $('#major').editable();
