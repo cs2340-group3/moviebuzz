aca77bf618506c40eb18fbadce1a51e6b549de43
Tim Vergenz <vergenzt@gmail.com>
2015-06-22 14:50:46 -0400

    Include default RottenTomatoes api key

--- config/rotten.js
+++ config/rotten.js
@@ -1,3 +1,3 @@
 var rotten = require('rotten-tomatoes-api');
-module.exports = rotten(process.env.ROTTEN_API_KEY);
+module.exports = rotten(process.env.ROTTEN_API_KEY || 'yedukp76ffytfuy24zsqk7f5');
 
