640387de047b9d9802bc3c4947cc0d9bc4a398e9
Tim Vergenz <vergenzt@gmail.com>
2015-06-22 15:41:44 -0400

    Add ability to rate movies

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,16 @@ var router = express.Router();
 // This is Dr. Water's API
 // TODO: Put this API key to config
 var rotten = require('rotten-tomatoes-api')('yedukp76ffytfuy24zsqk7f5');
+var Rating = require('../models/rating');
+
+// require authentication
+router.all('*', function(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/');
+  } else {
+    next();
+  }
+});
 
 function tomatoesError(err, loggedInfo, req) {
   res.status('400').render('error', {
@@ -19,12 +29,46 @@ router.get('/movie/:id', function(req, res) {
     if (err) {
       return tomatoesError(err, loggedInfo, req);
     }
-    console.log('test');
-    return res.render('movie', {
-      username: loggedInfo
-      , csrfToken: req.csrfToken()
-      , movie: val
+    Rating.findOne({ username: loggedInfo, movieId: id }, function(err, rating) {
+      if (err) {
+        return res.status('400').render('error', {
+          username: loggedInfo
+          , csrfToken: req.csrfToken()
+          , message: err
+        });
+      }
+
+      return res.render('movie', {
+        username: loggedInfo
+        , csrfToken: req.csrfToken()
+        , movie: val
+        , userRating: rating
+      });
     });
+  });
+});
+
+router.post('/movie/:id/rate', function(req, res) {
+  var username = req.user.username;
+  var movieId = req.params.id;
+  var score = req.body.score;
+  var review = req.body.review;
+  console.log('test');
+
+  var query = { username: username, movieId: movieId };
+  var newDocument = 
+    { username: username, movieId: movieId, score: score, review: review };
+
+  return Rating.findOneAndUpdate(query, newDocument, {upsert: true}, function(err, movie) {
+    var loggedInfo = req.user ? req.user.username : "";
+    if (err) {
+      return res.status('400').render('error', {
+        username: loggedInfo
+        , csrfToken: req.csrfToken()
+        , message: err
+      });
+    }
+    return res.redirect('/movie/' + movieId);
   });
 });
 
@@ -48,6 +92,9 @@ router.get('/search/:keyword', function (req, res) {
 });
 
 router.get('/recent/dvd', function (req, res) {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/');
+  }
   var loggedInfo = req.user ? req.user.username : "";
   return rotten.listDvdsNewReleases({ page_limit: 20 }, function(err, val) {
     if (err) {
