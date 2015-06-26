01adf41a123ba9e0d83f5d6464f2cb42cef5e6dc
kcaras6 <caraskimberly@yahoo.com>
2015-06-25 22:23:12 -0400

    recent movies on welcome

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var rotten = require('rotten-tomatoes-api')('yedukp76ffytfuy24zsqk7f5');
 
 // Set up the root route
 router.get('/', function(req, res) {
   var loggedInfo = req.user ? req.user.username : "";
-  res.render('welcome', {
-    username: loggedInfo
-    , csrfToken: req.csrfToken()
+  return rotten.listDvdsNewReleases({ page_limit: 20 }, function(err, val) {
+    if (err) {
+      return tomatoesError(err, loggedInfo, req);
+    }
+    return res.render('movies', {
+      username: loggedInfo
+      , csrfToken: req.csrfToken()
+      , movies: val.movies
+    });
   });
 });
 
 module.exports = router;
-
